6f4d2fc8c0da2afa5723acca33e67f4e735766d4
Justin Riley <justin.t.riley@gmail.com>
2009-07-29 17:32:46 -0400

    switched to simpler example of rendering text
    renamed logger instance in code/main.py to log

--- contents/code/main.py
+++ contents/code/main.py
@@ -4,8 +4,8 @@ from PyQt4.QtGui import *
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
 import logging
-from logger import logger
-logger.setLevel(logging.DEBUG)
+from logger import logger as log
+log.setLevel(logging.DEBUG)
  
 class AWSPlasmoid(plasmascript.Applet):
     def __init__(self,parent,args=None):
@@ -13,27 +13,36 @@ class AWSPlasmoid(plasmascript.Applet):
  
     def init(self):
         self.setHasConfigurationInterface(False)
-        self.resize(125, 125)
         self.setAspectRatioMode(Plasma.Square)
-        self.images = None
+ 
+        self.theme = Plasma.Svg(self)
+        self.theme.setImagePath("widgets/background")
+        self.setBackgroundHints(Plasma.Applet.DefaultBackground)
+ 
+        self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
+        label = Plasma.Label(self.applet)
+        label.setText("Hello world!")
+        self.layout.addItem(label)
+        self.setLayout(self.layout)
+        self.resize(125,125)
         self.connectToEngine()
  
-    def paintInterface(self, painter, option, rect):
-        painter.save()
-        painter.setPen(Qt.white)
-        painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "AWS Plasmoid!")
-        painter.restore()
+    #def paintInterface(self, painter, option, rect):
+        #painter.save()
+        #painter.setPen(Qt.white)
+        #painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "AWS Plasmoid!")
+        #painter.restore()
     
     def connectToEngine(self):
-        logger.debug("connectToEngine")
+        log.debug("connectToEngine")
         self.awsEngine = self.dataEngine("awsengine")
         self.awsEngine.connectSource("images", self, 6000, Plasma.AlignToMinute)
 
     @pyqtSignature("dataUpdated(const QString &, const Plasma::DataEngine::Data &)")
     def dataUpdated(self, sourceName, data):
-        logger.debug("dataUpdated")
+        log.debug("dataUpdated")
         self.images = data
-        logger.debug("self.images = %s" % self.images)
+        log.debug("self.images = %s" % self.images)
         self.update()
  
 def CreateApplet(parent):
